51278af59072533fe9f0665432a8b7cce26972c4
dhchoi <cdgus1514@gmail.com>
2023-07-31 17:05:29 +0900

    의존관계 주입방식으로 변경하기

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    /**
+     * 생성자 주입을 통해서 구현체를 결정. 해당 구현체는 인터페이스만을 의존(DIP)
+     * @return
+     */
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,17 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 인터페이스만 의존(구체적인 클래스를 몰라도 상관없음 - 의존관계 주입)
+     * db/memory/jdbc 중 어떤걸 넣어도 상관없이 사용가능
+     */
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
