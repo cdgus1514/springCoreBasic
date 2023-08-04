d3cfd89ec6d202363e5e2e6495f4ed75610e497a
dhchoi <cdgus1514@gmail.com>
2023-08-04 14:25:48 +0900

    설정정보 xml로 만들기

---
+++ core/src/test/java/hello/core/xml/XmlAppCOntext.java
@@ -0,0 +1,17 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppCOntext {
+
+    @Test
+    void xmlAppContext() {
+        // resources/appConfig.xml 파일을 사용
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
