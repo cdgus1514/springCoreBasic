bce09c0a900644a2cbbd6fd536d4b1cdb5c44a16
dhchoi <cdgus1514@gmail.com>
2023-08-16 21:36:45 +0900

    스프링 빈 생명주기 - 어노테이션

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 //public class NetworkClient implements InitializingBean, DisposableBean {
 
@@ -45,12 +48,14 @@ public class NetworkClient {
 //    }
 
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
