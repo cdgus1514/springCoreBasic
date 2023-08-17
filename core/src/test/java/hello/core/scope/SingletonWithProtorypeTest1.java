d7bcd8760a6e32a33fee9237b6f8abb928ba0963
dhchoi <cdgus1514@gmail.com>
2023-08-17 16:44:59 +0900

    빈 스코프 - 프로토타입, 싱글톤 provider1

--- core/src/test/java/hello/core/scope/SingletonWithProtorypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithProtorypeTest1.java
@@ -2,11 +2,14 @@ package hello.core.scope;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,7 +42,8 @@ public class SingletonWithProtorypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int logic2 = clientBean2.logic();
-        assertThat(logic2).isEqualTo(2);
+//        assertThat(logic2).isEqualTo(2);
+        assertThat(logic2).isEqualTo(1);
     }
 
 
@@ -47,11 +51,17 @@ public class SingletonWithProtorypeTest1 {
      * 생성자를 호출하면서 스프링 컨테이너에서 prototypeBean을 생성해서 반환
      */
     @Scope("singleton")
-    @RequiredArgsConstructor
+//    @RequiredArgsConstructor
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입
+//        private final PrototypeBean prototypeBean; // 생성시점에 주입
+
+
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); // 스프링 컨테이너를 통해 해당 빈을 찾아서 반환
+
             prototypeBean.addCount();
 
             return prototypeBean.getCount();
